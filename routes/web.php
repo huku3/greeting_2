cb4261a197805eba2d8d5c66a7f826cd581a7d14
huku3 <mhideyuki7777@icloud.com>
2024-09-09 12:06:02 +0900

    動作確認OK

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\GreetingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/comments/random',[GreetingController::class,'random']);
+
+Route::get('/comments/{operator}',[GreetingController::class,'greeting']);
+
+Route::get('/comments/freeword/{word}',[GreetingController::class,'freeword']);
+
 
 
